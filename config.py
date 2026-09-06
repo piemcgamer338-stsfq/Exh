48edb7d183ee8cc972a34654bbaa43fc916dc35e
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-09-06 20:48:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,48 @@
+# config.py
+
+PREFIX = "."
+
+# Exchange rates
+RATES = {
+    "I2C": "₹104/$",
+    "C2I": "₹100/$",
+    "C2C": "5% Fees",
+    "N2C": "165/$",
+    "C2N": "150/$",
+    "B2C": "142/$",
+    "C2B": "118/$",
+}
+
+CRYPTO = [
+    "ETH",
+    "LTC",
+    "BTC",
+    "SOL",
+    "USDT",
+    "BNB (BEP20)"
+]
+
+PAYMENT_METHODS = {
+    "INR": [
+        "Google Pay",
+        "Paytm",
+        "PhonePe",
+        "FamPay",
+        "Amazon Pay",
+        "Dhani"
+    ],
+
+    "NPR": [
+        "E-Sewa",
+        "Khalti",
+        "Prabhu Pay",
+        "IME Pay"
+    ],
+
+    "BDT": [
+        "bKash",
+        "Nagad",
+        "UPay",
+        "Rocket"
+    ]
+}
